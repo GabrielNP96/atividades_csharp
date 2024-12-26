a8b8d3596d7f6cabdd07900eeacbbf9ecb5e21af
Gabriel Nascimento <123754383+GabrielNP96@users.noreply.github.com>
2024-12-26 22:28:18 +0000

    maior ou menor

---
+++ 05HigherOrLower/Program.cs
@@ -0,0 +1,18 @@
+﻿//recebendo dados
+Console.Write("Digite um número: ");
+string firstNumberStr = Console.ReadLine()!;
+Console.Write("Digite outro número: ");
+string secondNumberStr = Console.ReadLine()!;
+
+//convertendo dados
+double firstNumber = double.Parse(firstNumberStr);
+double secondNumber = double.Parse(secondNumberStr);
+
+//verificando maior ou menor
+
+if(firstNumber > secondNumber)
+{
+    Console.WriteLine($"{firstNumber} é maior que {secondNumber}");
+} else {
+    Console.WriteLine($"{secondNumber} é maior que {firstNumber}");
+}
