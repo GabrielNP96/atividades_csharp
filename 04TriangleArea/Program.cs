6ba8eb9f1594d9166afd16220167f914d9c7b29f
Gabriel Nascimento <123754383+GabrielNP96@users.noreply.github.com>
2024-12-26 22:08:01 +0000

    Área do triângulo

---
+++ 04TriangleArea/Program.cs
@@ -0,0 +1,20 @@
+﻿//recebendo dados
+Console.Write("Vamos calcular a área de um triângulo!\nDigite a Base: ");
+string triangleBaseStr = Console.ReadLine()!;
+Console.Write("Digite a altura: ");
+string triangleHeightStr = Console.ReadLine()!;
+
+//convertendo
+double triangleBase = double.Parse(triangleBaseStr);
+double triangleHeight = double.Parse(triangleHeightStr);
+
+//calculando
+
+void TriagleArea(double triangleBase, double triangleHeight)
+{
+    Console.WriteLine($"A área do triângulo é {(triangleBase * triangleHeight) / 2}");
+}
+
+TriagleArea(triangleBase, triangleHeight);
+
+
