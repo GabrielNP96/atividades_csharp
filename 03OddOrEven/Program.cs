bf13cad990cf5fbe23f03eb0576292a718fbd9bf
Gabriel Nascimento <123754383+GabrielNP96@users.noreply.github.com>
2024-12-26 21:37:18 +0000

    par ou impar

---
+++ 03OddOrEven/Program.cs
@@ -0,0 +1,19 @@
+﻿//Recebendo o número do usuario e convertendo para int
+Console.Write("Vamos descobrir se um número é par ou ímpar.\nDigite um número inteiro positivo: ");
+string userNumberStr = Console.ReadLine()!;
+int userNumber = int.Parse(userNumberStr);
+
+//Descobrindo se é par
+
+void OddOrEven(int number)
+{
+    if(number % 2 == 0)
+    {
+        Console.WriteLine($"{number} é par");
+        return;
+    }
+
+    Console.WriteLine($"{number} é ímpar");
+}
+
+OddOrEven(userNumber);
--- 03OddOrEven/obj/Debug/net8.0/03OddOrEven.AssemblyInfo.cs
+++ 03OddOrEven/obj/Debug/net8.0/03OddOrEven.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("01PrimeNumbers")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("03OddOrEven")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc67f915050deb8bcc752b75ba400f77f5fbb5d7")]
-[assembly: System.Reflection.AssemblyProductAttribute("01PrimeNumbers")]
-[assembly: System.Reflection.AssemblyTitleAttribute("01PrimeNumbers")]
+[assembly: System.Reflection.AssemblyProductAttribute("03OddOrEven")]
+[assembly: System.Reflection.AssemblyTitleAttribute("03OddOrEven")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
