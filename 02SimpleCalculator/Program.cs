4392c87fc81dc7ae0343dd5499bd92aaf412a41b
Gabriel Nascimento <123754383+GabrielNP96@users.noreply.github.com>
2024-12-26 20:50:21 +0000

    nova atividade

---
+++ 02SimpleCalculator/Program.cs
@@ -0,0 +1,35 @@
+﻿//recebe dados do usuario
+Console.Write("Vamos fazer um calculo simples!\nDigite um número: ");
+string userNumber1 = Console.ReadLine()!;
+Console.Write("Digite um dos seguintes operadores (+, -, *, /) :");
+string mathOperator = Console.ReadLine()!;
+Console.Write("Digite mais um número: ");
+string userNumber2 = Console.ReadLine()!;
+
+//convertendo dados
+double firstNumber = double.Parse(userNumber1);
+double secondNumber = double.Parse(userNumber2);
+
+//calculando
+
+void Calculator(double number1, string mathOperator, double number2)
+{
+    if (mathOperator == "+")
+    {
+        Console.WriteLine($"{number1} + {number2} = {number1 + number2}");
+    } else if (mathOperator == "-")
+    {
+         Console.WriteLine($"{number1} - {number2} = {number1 - number2}");
+    } else if (mathOperator == "*")
+    {
+        Console.WriteLine($"{number1} * {number2} = {number1 * number2}");
+    } else if (mathOperator == "/")
+    {
+        Console.WriteLine($"{number1} / {number2} = {number1 / number2}");
+    } else
+    {
+        Console.WriteLine($"{mathOperator} não é um operador válido");
+    }
+}
+
+Calculator(firstNumber, mathOperator, secondNumber);
--- 02SimpleCalculator/obj/Debug/net8.0/02SimpleCalculator.AssemblyInfo.cs
+++ 02SimpleCalculator/obj/Debug/net8.0/02SimpleCalculator.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("01PrimeNumbers")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("02SimpleCalculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d21e13b08068aefaa96eb1b4520e1af20f23d68")]
-[assembly: System.Reflection.AssemblyProductAttribute("01PrimeNumbers")]
-[assembly: System.Reflection.AssemblyTitleAttribute("01PrimeNumbers")]
+[assembly: System.Reflection.AssemblyProductAttribute("02SimpleCalculator")]
+[assembly: System.Reflection.AssemblyTitleAttribute("02SimpleCalculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
