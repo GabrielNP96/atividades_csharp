3748a009c953df67da64f7d43ad3f1ded95a08da
Gabriel Nascimento <123754383+GabrielNP96@users.noreply.github.com>
2024-12-27 18:45:46 +0000

    .....

--- 06CurrencyConverter/Program.cs
+++ 06CurrencyConverter/Program.cs
@@ -1,25 +1,42 @@
 ﻿// Converter uma quantidade de dinheiro de uma moeda para outra.
-Console.WriteLine("Bem vindo ao Conversor de Moedas");
+Console.WriteLine("Bem vindo ao Conversor Real e dollar");
 bool i = true;
 while(i != false)
 {
     //recebendo dados
-    Console.WriteLine(@"Digite o númeral referente a moeda original: 
+    Console.WriteLine(@"Escolha para qual moeda converter:
                         1 - Dollar
-                        2 - Real
-                        3 - Euro
-                        4 - Libra Esterlina");
+                        2 - Real");
     string inputOptionStr = Console.ReadLine()!;
-    
-    Console.WriteLine(@"Digite o númeral referente a moeda a ser convertida: 
-                        1 - Dollar
-                        2 - Real
-                        3 - Euro
-                        4 - Libra Esterlina");
-    string exitOptionStr = Console.ReadLine()!;
+
+    Console.Write("Digite a quantidade a converter: ");
+    string valueToConvertStr = Console.ReadLine()!;
 
     //convertendo dados
     int inputOption = int.Parse(inputOptionStr);
-    int exitOption = int.Parse(exitOptionStr);
+    double valueToConvert = double.Parse(valueToConvertStr);
+
+    //realizando conversão
 }
+
+void Conversor(int valueInput,double valueToConvert)
+{
+    if(valueToConvert <= 0)
+    {
+        Console.WriteLine("0 não pode ser convertido");
+        return;
+    }else
+    {
+        switch (valueInput)
+        {
+            case 1:
+                Console.WriteLine($"{valueInput} em dolar equivale a {valueInput / 6,20} Reais");
+                break;
+            case 2:
+                Console.WriteLine($"{valueInput} em Reais equivale a {
+        }
+    }
+}
+
+
 
